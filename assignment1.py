87bc4336342dbaa56faec9611cff54d3ae00317d
Your Name <rsanjanaki@unomaha.edu>
2026-02-01 03:27:11 -0600

    4

--- assignment1.py
+++ assignment1.py
@@ -1,28 +1,37 @@
 import pandas as pd
 import numpy as np
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
+import os
 
-train_data = pd.read_csv('assignment_data_train.csv')
+if os.path.exists('assignment_data_train.csv'):
+    train_data = pd.read_csv('assignment_data_train.csv')
+else:
+    train_data = pd.read_csv('https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv')
+
 train_data['Timestamp'] = pd.to_datetime(train_data['Timestamp'])
 train_data.set_index('Timestamp', inplace=True)
 
 
 y_train = train_data['trips']
 
+
 model = ExponentialSmoothing(
     y_train,
-    seasonal_periods=168, 
+    seasonal_periods=168,  
     trend='add',
     seasonal='add',
     initialization_method='estimated'
 )
 
+
 modelFit = model.fit(optimized=True)
 
-test_data = pd.read_csv('assignment_data_test.csv')
+if os.path.exists('assignment_data_test.csv'):
+    test_data = pd.read_csv('assignment_data_test.csv')
+else:
+    test_data = pd.read_csv('https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_test.csv')
 
 pred = modelFit.forecast(steps=744)
 
-# Ensure pred is a numpy array
 pred = np.array(pred)
 
