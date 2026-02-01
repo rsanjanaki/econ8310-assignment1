0fee631da5a2ace5c24a70326ef89025b743b1aa
Your Name <rsanjanaki@unomaha.edu>
2026-02-01 16:35:52 -0600

    5

--- assignment1.py
+++ assignment1.py
@@ -3,35 +3,39 @@ import numpy as np
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 import os
 
-if os.path.exists('assignment_data_train.csv'):
-    train_data = pd.read_csv('assignment_data_train.csv')
+
+train_path = "assignment_data_train.csv"
+train_url = "https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv"
+
+if os.path.exists(train_path):
+    train_data = pd.read_csv(train_path)
 else:
-    train_data = pd.read_csv('https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv')
+    train_data = pd.read_csv(train_url)
+
 
 train_data['Timestamp'] = pd.to_datetime(train_data['Timestamp'])
-train_data.set_index('Timestamp', inplace=True)
+train_data = train_data.sort_values('Timestamp').set_index('Timestamp')
 
 
 y_train = train_data['trips']
 
 
+y_train.index.freq = y_train.index.inferred_freq
+
+
 model = ExponentialSmoothing(
     y_train,
-    seasonal_periods=168,  
-    trend='add',
-    seasonal='add',
+    seasonal_periods=168,        
+    trend='add',                  
+    seasonal='add',               
     initialization_method='estimated'
 )
 
 
 modelFit = model.fit(optimized=True)
 
-if os.path.exists('assignment_data_test.csv'):
-    test_data = pd.read_csv('assignment_data_test.csv')
-else:
-    test_data = pd.read_csv('https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_test.csv')
 
 pred = modelFit.forecast(steps=744)
 
-pred = np.array(pred)
 
+pred = np.array(pred)
