ef645b49bf9be8f5742ba62f60a311e100149caa
Your Name <rsanjanaki@unomaha.edu>
2026-02-01 21:29:59 -0600

    9

--- assignment1.py
+++ assignment1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
-from prophet import Prophet
+from pygam import LinearGAM, s, f
 
 
 train_path = "assignment_data_train.csv"
@@ -13,33 +13,36 @@ else:
     train_data = pd.read_csv(train_url)
 
 
-train_data["Timestamp"] = pd.to_datetime(train_data["Timestamp"])
-train_data = train_data.sort_values("Timestamp")
+train_data['Timestamp'] = pd.to_datetime(train_data['Timestamp'])
+train_data = train_data.sort_values('Timestamp')
 
 
-prophet_train = train_data.rename(columns={"Timestamp": "ds", "trips": "y"})[["ds", "y"]]
+x_train = train_data[['year', 'month', 'day', 'hour']].values
+y_train = train_data['trips'].values
 
 
-model = Prophet(
-    yearly_seasonality=False,      
-    weekly_seasonality=True,       
-    daily_seasonality=True,        
-    seasonality_mode="additive"   
+model = LinearGAM(s(0) + f(1) + f(2) + s(3))
 
 
-model.add_seasonality(name="hourly", period=24, fourier_order=10)
+
+modelFit = model.gridsearch(x_train, y_train)
 
 
-modelFit = model.fit(prophet_train)
+test_path = "assignment_data_test.csv"
+test_url = "https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_test.csv"
+
+if os.path.exists(test_path):
+    test_data = pd.read_csv(test_path)
+else:
+    test_data = pd.read_csv(test_url)
 
 
-future = model.make_future_dataframe(periods=744, freq="H")
+test_data['Timestamp'] = pd.to_datetime(test_data['Timestamp'])
+test_data = test_data.sort_values('Timestamp')
+x_test = test_data[['year', 'month', 'day', 'hour']].values
 
 
-forecast = modelFit.predict(future)
-
-
-pred = forecast["yhat"].tail(744).to_numpy()
+pred = modelFit.predict(x_test)
 
 
 pred = np.maximum(pred, 0)
