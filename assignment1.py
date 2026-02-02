908d5798a06a44958dccca88674dafd9ae210c29
Your Name <rsanjanaki@unomaha.edu>
2026-02-01 21:23:20 -0600

    8

--- assignment1.py
+++ assignment1.py
@@ -12,26 +12,31 @@ if os.path.exists(train_path):
 else:
     train_data = pd.read_csv(train_url)
 
+
 train_data["Timestamp"] = pd.to_datetime(train_data["Timestamp"])
 train_data = train_data.sort_values("Timestamp")
 
 
 prophet_train = train_data.rename(columns={"Timestamp": "ds", "trips": "y"})[["ds", "y"]]
 
-m = Prophet(
-    yearly_seasonality=False,
-    weekly_seasonality=True,
-    daily_seasonality=True,
-    seasonality_mode="additive"
-)
+
+model = Prophet(
+    yearly_seasonality=False,      
+    weekly_seasonality=True,       
+    daily_seasonality=True,        
+    seasonality_mode="additive"   
 
 
-m.add_seasonality(name="hourly", period=24, fourier_order=10)
+model.add_seasonality(name="hourly", period=24, fourier_order=10)
 
-m.fit(prophet_train)
 
-future = m.make_future_dataframe(periods=744, freq="H")
-forecast = m.predict(future)
+modelFit = model.fit(prophet_train)
+
+
+future = model.make_future_dataframe(periods=744, freq="H")
+
+
+forecast = modelFit.predict(future)
 
 
 pred = forecast["yhat"].tail(744).to_numpy()
